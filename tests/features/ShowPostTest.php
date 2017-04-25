37a0c3f6e126dcbddca4f07d5eb1998b6b18ba3b
Juan Palencia <j_palenci@yahoo.com>
2017-04-25 09:35:37 -0400

    Lesson 14 commit

--- tests/features/ShowPostTest.php
+++ tests/features/ShowPostTest.php
@@ -22,9 +22,30 @@ class ShowPostTest extends FeatureTestCase
         $user->posts()->save($post);
 
         // When
-        $this->visit(route('posts.show', $post))
+        $this->visit($post->url)
         	->seeInElement('h1', $post->title)
         	->see($post->content)
         	->see($user->name);
     }
+
+    public function test_old_url_are_redirected ()
+    {
+        //Having
+        $user = $this->defaultUser([
+        ]);
+        
+        $post = factory(\App\Post::class)->make([
+            'title' => 'Old title',
+        ]);
+
+        $user->posts()->save($post);
+        
+        $url = $post->url;
+
+        $post->update(['title' => 'New title']);     
+
+        $this->visit($url)
+            ->seePageIs($post->url);   
+    }
+
 }
